7db7e1c61a35987e5052bb9f597ed360034180b1
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2021-06-20 16:54:35 -0300

    feat: blood types enum

---
+++ src/common/constants/blood-type.ts
@@ -0,0 +1,12 @@
+enum BloodTypes {
+  O_POSITIVE = 'O+',
+  O_NEGATIVE = 'O-',
+  A_POSITIVE = 'A+',
+  A_NEGATIVE = 'A-',
+  B_POSITIVE = 'B+',
+  B_NEGATIVE = 'B-',
+  AB_POSITIVE = 'AB+',
+  AB_NEGATIVE = 'AB-',
+}
+
+export default BloodTypes;
