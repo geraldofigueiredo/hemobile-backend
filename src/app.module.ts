04d0c9fe59b7230d82321bed778b978bd6d55f98
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2021-06-20 16:56:24 -0300

    feat: users module on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
+import { UsersModule } from './domain/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
